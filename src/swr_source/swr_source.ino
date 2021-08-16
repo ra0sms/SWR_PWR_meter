8e59d6bb8f8bb0aba379f0eea1db8d4bb06dd9db
Anton Gavrilov RA0SMS <ra0sms@bk.ru>
2021-08-16 23:56:30 +0800

    add SVG
    add SVG

--- src/swr_source/swr_source.ino
+++ src/swr_source/swr_source.ino
@@ -14,9 +14,9 @@
 #include <Ticker.h>
 
 
-#define MAX_POWER 200 
+#define MAX_POWER 1000       //1000, 2000, 200 
 
-const char *softAP_ssid = "SWRmeter_sn049";
+const char *softAP_ssid = "SWRmeter_sn067";
 const char *softAP_password = "1234567890";
 const char *myHostname = "esp8266";
  
@@ -26,7 +26,7 @@ String webPage = "";
 String webpage = "";
 String ourPage = "";
 String Page3="";
-String webSite, javaScript, XML, progress, StringSWR;
+String webSite, javaScript, XML, progress, StringSWR, SVG;
  
 int switch_pin = 5;
 int led_pin = 16;
@@ -84,11 +84,11 @@ int readADC ()
 }
 
 int readMedian (){
-  int samples = 15;
+  int samples = 12;
   int raw[samples];
   for (int i = 0; i < samples; i++){
     raw[i] = analogRead(A0);
-    delayMicroseconds(100);
+    delayMicroseconds(200);
   }
   int temp = 0; 
   for (int i = 0; i < samples; i++){
@@ -143,16 +143,86 @@ void GetPower()
       StringSWR = floatToString(swr, 2);
       //delay(1);
     } else {
-      StringSWR = "TOO HIGH";
+      StringSWR = ">10";
       //delay(1);
     }
   } else
   {
     power = 0;
-    StringSWR = "NO DATA";
+    StringSWR = " ";
   }
 }
 
+void BuildSVG()
+{
+ SVG = "<svg version=\"1.0\" xmlns=\"http://www.w3.org/2000/svg\"";
+ SVG +=F("width=\"99.000000pt\" height=\"35.000000pt\" viewBox=\"0 0 790.000000 284.000000\""
+ "preserveAspectRatio=\"xMidYMid meet\">"
+"<g transform=\"translate(0.000000,284.000000) scale(0.100000,-0.100000)\""
+"fill=\"#000000\" stroke=\"none\">"
+"<path d=\"M0 1420 l0 -1420 910 0 910 0 0 1420 0 1420 -910 0 -910 0 0 -1420z "
+"m1620 0 l0 -1220 -710 0 -710 0 0 1220 0 1220 710 0 710 0 0 -1220z\"/>"
+"<path d=\"M392 1983 l3 -458 103 -3 102 -3 0 161 0 160 270 0 c176 0 277 -4 "
+"293 -11 39 -18 47 -50 47 -184 l0 -125 106 0 106 0 -4 153 c-3 173 -15 206 "
+"-80 241 l-35 20 31 21 c69 46 101 130 93 241 -8 110 -54 181 -142 221 -36 16 "
+"-81 18 -468 21 l-427 3 2 -458z m789 244 c48 -37 53 -115 10 -158 l-29 -29 "
+"-281 0 -281 0 0 100 0 100 283 0 c206 -1 286 -4 298 -13z\"/>"
+"<path d=\"M394 1294 c-4 -14 -4 -164 -1 -332 4 -275 7 -314 25 -372 25 -76 71 "
+"-127 147 -162 49 -23 57 -23 453 -26 l402 -3 0 460 0 461 -105 0 -105 0 0 "
+"-366 0 -366 -258 4 c-375 5 -350 -27 -351 451 l-1 277 -100 0 c-99 0 -100 0 "
+"-106 -26z\"/>"
+"<path d=\"M2210 1980 l0 -460 100 0 100 0 0 315 c0 173 3 315 6 315 4 0 62 -52 "
+"131 -116 123 -116 124 -117 145 -98 12 11 69 63 127 117 l106 98 3 -315 2 "
+"-316 100 0 100 0 0 460 0 460 -56 0 c-96 0 -129 -16 -233 -114 -53 -50 -113 "
+"-106 -133 -125 l-38 -35 -119 117 c-138 135 -175 156 -273 157 l-68 0 0 -460z\"/>"
+"<path d=\"M3530 2433 c-102 -17 -166 -63 -197 -143 -15 -39 -17 -93 -21 -407 "
+"l-4 -363 106 0 106 0 0 155 0 155 250 0 250 0 0 -155 0 -155 105 0 105 0 0 "
+"460 0 460 -337 -1 c-186 -1 -349 -4 -363 -6z m490 -303 l0 -110 -250 0 -250 0 "
+"1 78 c1 142 -1 141 282 142 l217 0 0 -110z\"/>"
+"<path d=\"M4410 1980 l0 -461 353 3 c400 5 420 8 481 79 55 64 61 104 61 379 0 "
+"275 -6 315 -61 379 -61 71 -81 74 -481 79 l-353 3 0 -461z m644 249 c14 -7 30 "
+"-25 36 -42 13 -39 13 -375 0 -414 -17 -48 -41 -53 -265 -53 l-205 0 0 260 0 "
+"260 205 0 c131 0 213 -4 229 -11z\"/>"
+"<path d=\"M5680 2433 c-106 -18 -176 -72 -200 -156 -17 -60 -25 -449 -11 -533 "
+"15 -83 41 -136 83 -167 66 -48 84 -51 426 -55 l322 -3 0 100 0 101 -292 0 "
+"c-161 0 -298 4 -305 8 -19 12 -33 60 -33 113 l0 49 250 0 250 0 0 95 0 95 "
+"-250 0 -250 0 0 44 c0 49 15 96 33 108 7 4 137 8 290 8 l277 0 0 100 0 100 "
+"-282 -1 c-156 -1 -294 -4 -308 -6z\"/>"
+"<path d=\"M6580 1980 l0 -460 105 0 105 0 0 460 0 460 -105 0 -105 0 0 -460z\"/>"
+"<path d=\"M6980 1980 l0 -460 100 0 100 0 2 302 3 302 101 -125 c55 -68 140 "
+"-173 188 -234 173 -216 202 -236 339 -243 l87 -4 0 461 0 461 -100 0 -100 0 0 "
+"-301 c0 -187 -4 -298 -9 -294 -9 5 -48 52 -310 377 -56 70 -119 141 -139 158 "
+"-49 40 -112 60 -194 60 l-68 0 0 -460z\"/>"
+"<path d=\"M2217 1314 c-4 -4 -7 -211 -7 -461 l0 -453 105 0 105 0 0 161 0 160 "
+"226 -3 226 -3 24 -28 c23 -27 24 -35 24 -158 l0 -129 105 0 105 0 0 143 c0 "
+"166 -14 213 -75 248 l-36 22 34 23 c19 13 45 45 58 71 20 40 24 63 24 143 0 "
+"105 -16 149 -72 202 -62 59 -84 62 -478 66 -198 2 -364 0 -368 -4z m672 -209 "
+"c34 -17 41 -33 41 -89 0 -33 -6 -49 -26 -70 l-27 -26 -228 0 -229 0 0 100 0 "
+"100 220 0 c174 0 227 -3 249 -15z\"/>"
+"<path d=\"M3310 992 c0 -191 5 -353 11 -388 16 -90 48 -137 116 -171 l58 -28 "
+"368 -3 367 -3 0 460 0 461 -105 0 -105 0 0 -366 0 -366 -217 4 c-120 3 -226 8 "
+"-236 12 -44 20 -47 47 -47 391 l0 325 -105 0 -105 0 0 -328z\"/>"
+"<path d=\"M4625 1315 c-5 -2 -26 -6 -46 -9 -54 -9 -126 -62 -157 -115 -25 -42 "
+"-27 -55 -27 -146 0 -83 4 -107 22 -140 28 -52 57 -79 118 -107 47 -22 63 -23 "
+"295 -26 135 -1 255 -5 268 -7 62 -14 82 -90 38 -142 l-24 -28 -336 -3 -336 -3 "
+"0 -95 0 -95 368 3 c349 3 369 4 412 24 101 47 145 124 145 254 0 137 -51 217 "
+"-164 262 -51 19 -79 22 -306 25 -289 5 -295 6 -295 77 0 28 6 48 17 57 12 11 "
+"86 15 342 19 l326 5 3 75 c2 41 0 85 -3 97 l-5 23 -323 -1 c-177 0 -326 -2 "
+"-332 -4z\"/>"
+"<path d=\"M5653 1306 c-130 -32 -201 -138 -191 -287 6 -82 46 -161 99 -196 70 "
+"-46 116 -53 349 -53 126 0 230 -4 247 -10 49 -17 68 -84 38 -127 -26 -37 -76 "
+"-43 -377 -43 l-288 0 0 -95 0 -95 322 0 c370 0 407 5 482 72 58 50 80 109 81 "
+"208 0 93 -17 141 -67 196 -71 77 -112 86 -397 92 -221 4 -245 6 -262 23 -26 "
+"26 -25 83 1 109 19 19 33 20 320 20 l300 0 0 100 0 100 -302 -1 c-207 0 -319 "
+"-5 -355 -13z\"/>"
+"<path d=\"M6580 860 l0 -460 105 0 105 0 0 460 0 460 -105 0 -105 0 0 -460z\"/>"
+"<path d=\"M7189 1310 c-93 -16 -158 -67 -190 -150 -10 -26 -14 -125 -17 -397 "
+"l-3 -363 105 0 106 0 0 155 0 155 250 0 250 0 0 -155 0 -155 105 0 105 0 0 "
+"460 0 460 -332 -1 c-183 -1 -354 -5 -379 -9z m501 -300 l0 -110 -250 0 -250 0 "
+"0 75 c0 82 12 118 44 133 12 6 111 11 239 11 l217 1 0 -110z\"/>"
+"</g>"
+"</svg>"); 
+}
 
 void ProgressBar () {
   progress = "<style>\n";
@@ -223,7 +293,7 @@ void buildJavascript() {
 void buildXML() {
   XML = "<?xml version='1.0'?>";
   XML += "<response>";
-  XML += "Output power: " + String(power, DEC) + "&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;" + " SWR: " + StringSWR;
+  XML += "PWR: " + String(power, DEC) + "&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;" + " SWR: " + StringSWR;
   XML += "<adcpower>";
   XML += String(power_per, DEC) + "%";
   XML += "</adcpower>\n";
@@ -238,10 +308,13 @@ void handleSWR ()
   ProgressBar();
   webPage = "<!DOCTYPE HTML><title>SWR/PWR Meter</title>\n";
   webPage += javaScript;
-  webPage += "<BODY onload='process()'>\n";
-  webPage += "<BR><h2>WI-FI SWR/PWR Meter by RA0SMS</h2><BR>\n";
+  webPage += "<BODY onload='process()'; bgcolor=\"#c7daed\" >\n";
+  if (flagOn == 1) webPage += "<h2><b><font color=\"Crimson\" face=\"Lucida Console\"><A id='runtime'></A></font></b></h2>\n";
+  else webPage += "<BR><h2><a href='/wifi'>Need connecting to local wi-fi network</a></h2><BR>\n";
   webPage += progress;
-  webPage += "<h3><A id='runtime'></A></h3>\n";
+  if (MAX_POWER == 1000) webPage += "<BR><h3>SWR/PWR Meter by RA0SMS (ver. 1000W)</h3><BR>\n"; 
+  if (MAX_POWER == 200) webPage += "<BR><h3>SWR/PWR Meter by RA0SMS (ver. 200W)</h3><BR>\n"; 
+  if (MAX_POWER == 2000) webPage += "<BR><h3>SWR/PWR Meter by RA0SMS (ver. 2000W)</h3><BR>\n"; 
   webPage += "</BODY>\n";
   webPage += "</HTML>\n"; 
   server.send(200, "text/html", webPage);
@@ -249,6 +322,7 @@ void handleSWR ()
 
 void handleRoot() {
   String Page;
+  BuildSVG();
   server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   server.sendHeader("Pragma", "no-cache");
   server.sendHeader("Expires", "-1");
@@ -265,10 +339,10 @@ void handleRoot() {
     Page +=(String("<p>You are connected through the wifi network: ") + ssid + "</p>");
   }
   Page += F(
-    "<p><a href='/wifi'>Config the wifi connection</a>.</p>"
-    "<p><a href='/swr'>SWR/PWR meter page</a>.</p>"
-    "</body></html>"
-  );
+    "<p><a href='/wifi'>Config the wifi connection</a></p>"
+    "<p><a href='/swr'>SWR/PWR meter page</a></p>");
+    Page += SVG;
+    Page += "</body></html>";
   server.send(200, "text/html", Page);
 }
 
